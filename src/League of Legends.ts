5855bea23d0780c734792a33a47004fb4d042f8a
Dr-Fabian <dars.fabian@gmail.com>
2021-08-06 22:11:53 +0200

    👍 Reworked 'LeagueClient#getPlayer()' to accept a server parameter, thus avoiding the need to always query each server.

--- src/League of Legends.ts
+++ src/League of Legends.ts
@@ -4,11 +4,12 @@ import { Summoner } from "./typings/ISummoner";
 
 class LeagueClient extends RiotClient {
 
-  private servers: Array<string> = new Array<string>(11);
+  private servers_v4: Array<string> = new Array<string>(11);
+  //private servers_v5: Array<string> = new Array<string>(3);
 
   constructor(token: string) {
     super(token);
-    this.servers = [
+    this.servers_v4 = [
       "euw1",
       "eun1",
       "br1",
@@ -24,15 +25,24 @@ class LeagueClient extends RiotClient {
   }
 
   /**
-   * 
+   * Tries to get a player from its username. If it doesn't get any, it will return null.
+   * <b> Prefer providing a server parameter as the function will automatically try to get a valid '200 OK' reponse by querying each server if you don't. </b>
    * @param username username of the player.
+   * @param server server to query. (null by default).
    * @returns Instance of {@link Summoner}
    */
-  async getPlayer(username: string): Promise<Summoner | null> {
+  async getPlayer(username: string, server: string = null): Promise<Summoner | null> {
     username = username.trim();
     if(username == "" || username == null) throw new Error("The username cannot be empty or null!");
-    for(let i = 0; i < this.servers.length; i++) {
-      const reponse: AxiosResponse = await axios.get(`https://${this.servers[i]}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.token}`);
+
+    if(server != null) {
+      const reponse: AxiosResponse = await axios.get(`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.token}`);
+      if(reponse.status != 403) return reponse.data as Summoner;
+      else return null;
+    }
+
+    for(let i = 0; i < this.servers_v4.length; i++) {
+      const reponse: AxiosResponse = await axios.get(`https://${this.servers_v4[i]}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.token}`);
       if(reponse.status != 403) return reponse.data as Summoner;
     }
     return null;
